cc51936a82ea5424689c48d0676ca41d66aa112a
John Iberuoluwa Omosule <omosulejohn@gmail.com>
2024-08-22 03:07:48 -0600

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -21,6 +21,7 @@ public class OrderDao {
                 new Order(113, "Headphones", 2, 500),
                 new Order(114, "Refrigerator", 5, 2500),
                 new Order(115, "Play station", 3, 1500),
+                new Order(117, "Car", 3, 1600),
                 new Order(116, "Watch", 2, 1200))
             
                 .collect(Collectors.toList());
